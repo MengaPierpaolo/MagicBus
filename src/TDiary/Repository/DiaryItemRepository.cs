b7965336699fbb37ab040a22d933c8368fe56363
j_a_k <j_a_kimber@hotmail.com>
2016-06-05 19:52:44 +0900

    Issue #7.  Moving Dates - ensure ordering

--- src/TDiary/Repository/DiaryItemRepository.cs
+++ src/TDiary/Repository/DiaryItemRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using TDiary.Model;
@@ -15,15 +16,12 @@ namespace TDiary.Repository
 
         public void Add(DiaryItem item)
         {
-            var lastItem = _context.Experiences
-                .Where(i => i.Date.Date == item.Date.Date)
-                .OrderByDescending(i => i.SavePosition)
-                .FirstOrDefault();
-            
+            var lastItem = GetLastItem(item.Date);
             if (lastItem != null)
             {
                 item.MovePositionTo(lastItem.SavePosition + 1);
             }
+
             _context.Experiences.Add(item);
             _context.SaveChanges();
         }
@@ -43,8 +41,26 @@ namespace TDiary.Repository
 
         public void SaveChanges(DiaryItem item)
         {
+            var currentState = _context.Experiences.AsNoTracking().Where(di => di.Id == item.Id).Single();
+            if (item.Date.Date != currentState.Date.Date)
+            {
+                 var lastItem = GetLastItem(item.Date);
+                 if (lastItem != null)
+                 {
+                     item.MovePositionTo(lastItem.SavePosition + 1);
+                 }
+            }
+            
             _context.Attach(item).State = EntityState.Modified;
             _context.SaveChanges();
+        }
+
+        private DiaryItem GetLastItem(DateTime date)
+        {
+            return _context.Experiences
+                .Where(i => i.Date.Date == date)
+                .OrderByDescending(i => i.SavePosition)
+                .FirstOrDefault();
         }
     }
 }
