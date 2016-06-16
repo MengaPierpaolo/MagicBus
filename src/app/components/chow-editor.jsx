561a1ba8c50a3db02179cb6f4816c93e2cb0cee5
j_a_k <j_a_kimber@hotmail.com>
2016-06-16 21:42:47 +0900

    added further fields to the chow editor

--- src/app/components/chow-editor.jsx
+++ src/app/components/chow-editor.jsx
@@ -4,15 +4,13 @@ import Button from 'react-bootstrap/lib/Button';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
-
 import { Link } from 'react-router'
-
 import * as ChowActions from '../actionCreators/ChowActions';
 
 export default class ChowEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { description: 'Hello' };
+        this.state = { on: '', location: '', description: '' };
     }
 
     onClicked() {
@@ -20,22 +18,40 @@ export default class ChowEditor extends React.Component {
         this.props.history.pushState(null, '/'); // TODO: Deprecated - move to new.
     }
 
-    onChange(e) {
+    onChangeOn(e) {
+        this.setState({ date: e.target.value });
+    }
+    onChangeLocation(e) {
+        this.setState({ location: e.target.value });
+    }
+    onChangeDescription(e) {
         this.setState({ description: e.target.value });
     }
 
     render() {
         return (
-            <form>
-                <FormGroup controlId="formBasicText">
-                    <ControlLabel>Working example with validation</ControlLabel>
-                    <FormControl type="text"value={this.state.description} placeholder="Enter text" onChange={this.onChange.bind(this) } />
-                    <FormControl.Feedback />
-                </FormGroup>
+            <div className='Content'>
+                <form>
+                    <FormGroup>
+                        <ControlLabel>On</ControlLabel>
+                        <FormControl type="text"value={this.state.date} onChange={this.onChangeOn.bind(this) } />
+                        <FormControl.Feedback />
+                    </FormGroup>
+                    <FormGroup>
+                        <ControlLabel>When you were in</ControlLabel>
+                        <FormControl type="text"value={this.state.location} onChange={this.onChangeLocation.bind(this) } />
+                        <FormControl.Feedback />
+                    </FormGroup>
+                    <FormGroup>
+                        <ControlLabel>You consumed</ControlLabel>
+                        <FormControl type="text"value={this.state.description} onChange={this.onChangeDescription.bind(this) } />
+                        <FormControl.Feedback />
+                    </FormGroup>
 
-                <Button className="btn btn-primary" onClick={this.onClicked.bind(this) }>Save It!</Button>
-                <Link to="/" className="btn btn-info">Changed my mind</Link>
-            </form>
+                    <Button className="btn btn-primary" onClick={this.onClicked.bind(this) }>Save It!</Button>
+                    <Link to="/" className="btn btn-info">Changed my mind</Link>
+                </form>
+            </div>
         );
     }
 }
