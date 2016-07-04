54fabf7904020a1ca872991c068fd6753f157d4b
j_a_k <j_a_kimber@hotmail.com>
2016-07-04 20:01:32 +0900

    Preserve Current Page on Language Change

--- src/TDiary/Localization/LanguageActionFilter.cs
+++ src/TDiary/Localization/LanguageActionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace TDiary
@@ -7,17 +8,24 @@ namespace TDiary
     {
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            string culture = context.RouteData.Values["culture"]?.ToString()??string.Empty;
+            string culture = context.RouteData.Values["culture"]?.ToString() ?? string.Empty;
 
-            if (culture == "en-GB" || culture == "en-US" || culture == "zh-CN")
+            if (culture != "en-US" && culture != "zh-CN")
             {
-                CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(culture);
-                CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(culture);
+                culture = "en-GB";
             }
-            else
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(culture);
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(culture);
+
+            var c = context.Controller as Controller;
+            if (c != null)
             {
-                CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-GB");
-                CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-GB");
+                c.ViewData["culture"] = culture;
+                if (c.Request.Path.ToString().Contains("/") && c.Request.Path.ToString().Length > 1)
+                {
+                    c.ViewData["currentPage"] = c.Request.Path.ToString().Substring(c.Request.Path.ToString().IndexOf("/", 2) + 1);
+                }
             }
 
             base.OnActionExecuting(context);
