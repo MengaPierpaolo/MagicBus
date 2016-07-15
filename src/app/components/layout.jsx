e538c74a36286d57769bbf4146a652103f4a936e
j_a_k <j_a_kimber@hotmail.com>
2016-07-15 23:06:27 +0900

    Added favicon, navbar placeholder, latest css.

--- src/app/components/layout.jsx
+++ src/app/components/layout.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Toolbar from './toolbar'
+
 import Title from './title'
 import Dashboard from './dashboard'
 
@@ -6,6 +8,7 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
+        <Toolbar></Toolbar>
         <Title title="Magic Bus" subTitle="Your groovy new travel diary - in ReactJs!" />
         {this.props.children}
       </div>
